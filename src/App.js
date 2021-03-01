dac0d72d8b52f7293899386fe84d0493d07fad25
Manish Trivedi <manishtrivedi994@gmail.com>
2021-03-01 23:47:15 +0530

    introducing useEffect()

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { FaStar } from 'react-icons/fa'
 
-const createArray = (length) => [
-  ...Array(length)
-]
-
-function Star({ selected=false, onSelect }) {
-  return <FaStar color={selected? "red" : "gray"} onClick={onSelect}/>
-}
-
-function StarRating({ totalStars=5 }) {
-  const [selectedStars, setSelectedStars] = useState(0);
-  return <>
-    {createArray(totalStars).map((n, i) => (<Star key={i} selected={selectedStars>i} onSelect={() => setSelectedStars(i+1)}/>))}
-    <p>{selectedStars} of {totalStars}</p>
-  </>
-
-}
 function App() {
-    return (
-      <StarRating totalStars={10}/>
+  const [name, setName] =useState("Jan");
+  
+  useEffect(() => {
+    document.title = `Celebrate ${name}`
+  });
+  
+  return (
+      <section>
+        <p>
+          Congratulations {name}!
+        </p>
+        <button onClick={() => setName("WIll")}>Change Winner</button>
+      </section>
   );
 }
 
