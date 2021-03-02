bb7559a6a2d71b343a8317005f23c3689ee81ffd
Manish Trivedi <manishtrivedi994@gmail.com>
2021-03-03 01:50:55 +0530

    fetching data with useEffect()

--- src/App.js
+++ src/App.js
@@ -2,27 +2,28 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [name, setName] =useState("Jan");
-  const [admin, setAdmin] = useState(false);
-  
+  const [data, setData] = useState([]);
   useEffect(() => {
-    console.log(`Celebrate ${name}`)
-  }, [name]);
+    fetch(`https://api.github.com/users`)
+    .then(response => response.json())
+    .then(res => setData(res));
+  }, [])
 
-  useEffect(() => {
-    console.log(`The user is:  ${admin ? "admin" : "not admin"}.  `)
-  }, [admin]);
-
-  return (
-      <section>
-        <p>
-          Congratulations {name}!
-        </p>
-        <button onClick={() => setName("WIll")}>Change Winner</button>
-        <p>{admin ? "logged in" : "not logged in"}</p>
-        <button onClick={() => setAdmin(true)}>Log in</button>
-      </section>
-  );
+  if(data) {
+    return (
+      <div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            {user.login}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => setData([])}>Remove Data</button>
+      </div>
+    );
+  }
+  return <p>No users</p>
 }
 
 export default App;
